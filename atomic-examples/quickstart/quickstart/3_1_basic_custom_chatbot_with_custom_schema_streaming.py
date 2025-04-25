46a809e32ec6f22fa01865a38340ee01a2d4dee5
Koichi KAWAMURA <koichikwmr@icloud.com>
2025-04-25 16:02:42 +0900

    flake8 opt-out W503

--- atomic-examples/quickstart/quickstart/3_1_basic_custom_chatbot_with_custom_schema_streaming.py
+++ atomic-examples/quickstart/quickstart/3_1_basic_custom_chatbot_with_custom_schema_streaming.py
@@ -65,7 +65,7 @@ system_prompt_generator = SystemPromptGenerator(
         "Generate 3 suggested follow-up questions for the user to explore the topic further.",
         "When you get a simple number from the user,"
         "choose the corresponding question from the last list of suggested questions and answer it."
-        "Note that the first question is 1, the second is 2, and so on."
+        "Note that the first question is 1, the second is 2, and so on.",
     ],
     output_instructions=[
         "Provide clear, concise, and accurate information in response to user queries.",
